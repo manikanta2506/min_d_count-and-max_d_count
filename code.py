9d90e848d2801dabf54f8cd56c85c1fe3c3aa446
manikanta2506 <84144539+manikanta2506@users.noreply.github.com>
2021-05-14 12:09:55 +0530

    Add files via upload

--- code.py
+++ code.py
@@ -21,27 +21,4 @@ while temp:
     elif a==mi:
         c2=c2+1
 print(c2,c1)
-"""
-num=int(input())
-temp=num
-mi=9
-ma=0
-c=0
-d=0
-while num:
-    r=num%10
-    num=num//10
-    if r>ma:
-        ma=r
-    elif r<mi:
-        mi=r
-print(mi,ma)
-while temp:
-    r=temp%10
-    temp=temp//10
-    if r==ma:
-        c=c+1
-    elif r==mi:
-        d=d+1
-print(d,c)
-"""
+
